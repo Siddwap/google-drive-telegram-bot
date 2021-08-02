23de4dcc48c5ce9a3e7196791b67f3055376ef58
adarshgoel05 <86728094+adarshgoel05@users.noreply.github.com>
2021-08-02 22:17:24 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -67,7 +67,7 @@ class Messages:
     
     INVALID_AUTH_CODE = '❗ **Invalid Code**\n__The code you have sent is invalid or already used before. Generate new one by the Authorization URL__'
     
-    AUTH_TEXT = "⛓️ **To Authorize your Google Drive account visit this [URL]({}) and send the generated code here.**\n__Visit the URL > Allow permissions > you will get a code > copy it > Send it here__"
+    AUTH_TEXT = "⛓️ **To Authorize your Google Drive account visit this [URL]({}) and send the generated code here.**\n__Visit the URL > Allow permissions > you will get a code > copy it > Send it here   YOU MAY GET A MESSAGE THAT IT IS IS NOT SAFE GET BACK TO SAFET SIMPLY CLICK ADVANCED  AND  THEN CLICK ON 'GO TO ADARSH G DRIVE(unsafe)'__"
     
     DOWNLOAD_TG_FILE = "📥 **Downloading File...**\n**Filename:** ```{}```\n**Size:** ```{}```\n**MimeType:** ```{}```"
     
